440a5241318f1f898a95bbfc8d47ad1570504c1a
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-03-16 19:50:23 +0100

    Add register controller

--- src/api/auth/auth-controllers.ts
+++ src/api/auth/auth-controllers.ts
@@ -1,8 +1,41 @@
 import { RequestHandler } from 'express';
+import { CustomHTTPError } from '../../errors/custom-http-error.js';
 import log from '../../logger.js';
-import { UserModel } from '../users/user-schema.js';
-import { AuthRequest, LoginResponse } from './auth-types.js';
+import { User, UserModel } from '../users/user-schema.js';
+import { AuthRequest, LoginResponse, RegRequest } from './auth-types.js';
 import { encryptPassword, generateJWTToken } from './auth-utils.js';
+
+export const registerController: RequestHandler<
+  unknown,
+  unknown,
+  RegRequest
+> = async (req, res, next) => {
+  const { email, password, firstName, surName } = req.body;
+  try {
+    const existingUser = await UserModel.findOne({ email }).exec();
+    if (existingUser !== null) {
+      log.error('This email is already registered');
+      throw new CustomHTTPError(409, 'That email is already registered');
+    }
+
+    const registerUser: User = {
+      email,
+      password: encryptPassword(password),
+      phone: '',
+      firstName,
+      surName,
+      profileImg: '',
+    };
+
+    await UserModel.create(registerUser);
+    log.info('New user created');
+    return res
+      .status(201)
+      .json({ msg: 'El usuario se ha creado correctamente' });
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const loginUserController: RequestHandler<
   unknown,
