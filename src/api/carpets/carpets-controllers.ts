7aecfa9a596b90d99e60379d6023636120dd0926
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-03-20 19:58:09 +0100

    Add create carpets controller and tests

--- src/api/carpets/carpets-controllers.ts
+++ src/api/carpets/carpets-controllers.ts
@@ -1,4 +1,8 @@
 import { RequestHandler } from 'express';
+import {
+  PROFILE_BUCKET_NAME,
+  supabase,
+} from '../../database/supabase-client.js';
 import log from '../../logger.js';
 import { Carpet, CarpetModel } from './carpets-schema.js';
 
@@ -7,21 +11,36 @@ export const createCarpetController: RequestHandler<
   unknown,
   Carpet
 > = async (req, res, next) => {
-  const { name, thumb, price, width, height } = req.body;
-  try {
-    const createCarpet: Carpet = {
-      name,
-      thumb,
-      price,
-      width,
-      height,
-    };
+  const { name, price, width, height } = req.body;
 
-    await CarpetModel.create(createCarpet);
-    log.info('New carpet created');
-    return res
-      .status(201)
-      .json({ msg: 'Tu alfombra se ha tejido correctamente' });
+  try {
+    const fileBuffer = req.file?.buffer;
+    if (fileBuffer !== undefined) {
+      const fileName = `${name}.png`;
+      const { error } = await supabase.storage
+        .from(PROFILE_BUCKET_NAME)
+        .upload(fileName, fileBuffer);
+
+      if (error === null) {
+        const { data } = supabase.storage
+          .from(PROFILE_BUCKET_NAME)
+          .getPublicUrl(fileName);
+
+        const createCarpet: Carpet = {
+          name,
+          thumb: data.publicUrl,
+          price,
+          width,
+          height,
+        };
+
+        await CarpetModel.create(createCarpet);
+        log.info('New carpet created');
+        return res
+          .status(201)
+          .json({ msg: 'Tu alfombra se ha tejido correctamente' });
+      }
+    }
   } catch (err) {
     next(err);
   }
